8a9920495a4a2b8b2e9e66db18af848512196698
lichin-lin <vic20087cjimlin@gmail.com>
2022-05-17 12:27:53 -0700

    feat: cleanup

--- widget-src/code.tsx
+++ widget-src/code.tsx
@@ -19,20 +19,18 @@ function Widget() {
       figma.ui.postMessage({ type: "networkRequest", keyword: text });
 
       figma.ui.on("message", (msg) => {
-        const { type, width, height } = msg
+        const { type, width, height } = msg;
         if (type === "send-gif") {
-          figma.notify(`Pasting Gif...`);
           const { imgSrc, width, height } = msg?.value;
           const image = figma.createImage(imgSrc);
           const image_str = image.hash;
-          
+
           const rect = figma.createGif(image_str);
-          rect.resize(width, height)
+          rect.resize(width, height);
           figma.currentPage.appendChild(rect);
           figma.currentPage.selection = [rect];
           figma.viewport.scrollAndZoomIntoView([rect]);
-
-          figma.closePlugin();
+          figma.notify(`success!`);
         }
       });
     });
@@ -41,16 +39,16 @@ function Widget() {
     <AutoLayout padding={10} spacing={8}>
       <Input
         value={text}
-        placeholder="Type word...ex: dog"
+        placeholder="dog, sun, rainbow..."
         onTextEditEnd={(e) => {
           setText(e.characters);
         }}
-        fontSize={36}
+        fontSize={20}
         fill="#3d3d3d"
-        width={500}
+        width={256}
         inputFrameProps={{
-          fill: "#ead6e8",
-          stroke: "#9a59a8",
+          fill: "#e5d0ff",
+          stroke: "#9747ff",
           strokeWidth: 2,
           cornerRadius: 16,
           padding: 20,
@@ -59,15 +57,23 @@ function Widget() {
       />
       <AutoLayout
         fill="#f6f6f6"
-        width={"hug-contents"}
+        width={80}
         height="fill-parent"
         cornerRadius={16}
         verticalAlignItems="center"
         horizontalAlignItems="center"
         stroke="#e6e6e6"
         onClick={HandlePasteGifOnCanvas}
+        padding={{
+          top: 8,
+          left: 8,
+          bottom: 8,
+          right: 8
+        }}
       >
-        <Text fontSize={24}>go</Text>
+        <Text fontSize={20} width={24}>
+          ->
+        </Text>
       </AutoLayout>
     </AutoLayout>
   );
